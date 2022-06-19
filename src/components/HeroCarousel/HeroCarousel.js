1c4e60222952a1255ba42e4d3b1059bfb27683fc
Raf <rafaelmendoza004@gmail.com>
2022-06-19 13:07:44 +0800

    update

--- src/components/HeroCarousel/HeroCarousel.js
+++ src/components/HeroCarousel/HeroCarousel.js
@@ -1,135 +1,135 @@
-import sampleImage1 from "../../assets/images/building2.jpg";
+// import sampleImage1 from "../../assets/images/building2.jpg";
 import sampleImage2 from "../../assets/images/building.png";
-import sampleImage3 from "../../assets/images/building3.jpg";
+// import sampleImage3 from "../../assets/images/building3.jpg";
 
 import "tw-elements";
 
 const HeroCarousel = () => {
-  return (
-    <div
-      id="carouselExampleCaptions"
-      className="carousel slide relative "
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-2">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="0"
-          className="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-      <div className="carousel-inner relative w-full h-auto pb-6 overflow-hidden bg-insight ">
-        <div className="carousel-item active relative float-left w-full  ">
-          <div className="flex justify-center flex-col m-6 boxshadow">
-            <div className="mb-5">
-              <h2 className="text-white text-2xl">ACME Insights</h2>
-              <p className="text-white ">
-                How are factors being used around the world?
-              </p>
-            </div>
-            <div className="shadow-lg bg-white  p-5">
-              <a href="#!">
-                <img className="rounded-t-lg clippy " src={ sampleImage2 } alt="" />
-              </a>
-              <div className="p-1">
-                <h5 className="text-green-700 text-2xl font-semibold">
-                  Global Factor
-                </h5>
-                <p className="text-green-700 text-2xl mb-4 font-semibold">
-                  Investing Study
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item relative float-left  w-full">
-          <div className="flex justify-center flex-col  m-6 boxshadow">
-            <div className="mb-5">
-              <h2 className="text-white text-2xl">ACME Insights</h2>
-              <p className="text-white">
-                How are factors being used around the world?
-              </p>
-            </div>
-            <div className="shadow-lg bg-white  p-5">
-              <a href="#!">
-                <img className="rounded-t-lg clippy" src={ sampleImage2 } alt="" />
-              </a>
-              <div className="p-1">
-                <h5 className="text-green-700 text-2xl font-semibold ">
-                  Global Factor
-                </h5>
-                <p className="text-green-700 font-semibold mb-4 text-2xl">
-                  Investing Study
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item relative float-left w-full">
-          <div className="flex justify-center flex-col m-6 boxshadow">
-            <div className="mb-5">
-              <h2 className="text-white text-2xl">ACME Insights</h2>
-              <p className="text-white">
-                How are factors being used around the world?
-              </p>
-            </div>
-            <div className="shadow-lg bg-white  p-5">
-              <a href="#!">
-                <img className="rounded-t-lg clippy" src={ sampleImage2 } alt="" />
-              </a>
-              <div className="p-1">
-                <h5 className="text-green-700 text-2xl font-semibold">
-                  Global Factor
-                </h5>
-                <p className="text-green-700 text-2xl mb-4 font-semibold">
-                  Investing Study
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <button
-        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide="prev"
-      >
-        <span
-          className="carousel-control-prev-icon inline-block bg-no-repeat"
-          aria-hidden="true"
-        ></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-        type="button"
-        data-bs-target="#carouselExampleCaptions"
-        data-bs-slide="next"
-      >
-        <span
-          className="carousel-control-next-icon inline-block bg-no-repeat"
-          aria-hidden="true"
-        ></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
-  );
+	return (
+		<div
+			id="carouselExampleCaptions"
+			className="carousel slide relative "
+			data-bs-ride="carousel"
+		>
+			<div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 ">
+				<button
+					type="button"
+					data-bs-target="#carouselExampleCaptions"
+					data-bs-slide-to="0"
+					className="active"
+					aria-current="true"
+					aria-label="Slide 1"
+				></button>
+				<button
+					type="button"
+					data-bs-target="#carouselExampleCaptions"
+					data-bs-slide-to="1"
+					aria-label="Slide 2"
+				></button>
+				<button
+					type="button"
+					data-bs-target="#carouselExampleCaptions"
+					data-bs-slide-to="2"
+					aria-label="Slide 3"
+				></button>
+			</div>
+			<div className="carousel-inner relative w-full overflow-hidden bg-insight ">
+				<div className="carousel-item active relative float-left w-full  ">
+					<div className="flex justify-center flex-col m-6 boxshadow">
+						<div className="mb-5">
+							<h2 className="text-white text-2xl">ACME Insights</h2>
+							<p className="text-white ">
+								How are factors being used around the world?
+							</p>
+						</div>
+						<div className="shadow-lg bg-white  p-5">
+							<a href="#!">
+								<img className="rounded-t-lg clippy " src={sampleImage2} alt="" />
+							</a>
+							<div className="p-1">
+								<h5 className="text-green-700 text-2xl font-semibold">
+									Global Factor
+								</h5>
+								<p className="text-green-700 text-2xl mb-4 font-semibold">
+									Investing Study
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="carousel-item relative float-left  w-full">
+					<div className="flex justify-center flex-col  m-6 boxshadow">
+						<div className="mb-5">
+							<h2 className="text-white text-2xl">ACME Insights</h2>
+							<p className="text-white">
+								How are factors being used around the world?
+							</p>
+						</div>
+						<div className="shadow-lg bg-white  p-5">
+							<a href="#!">
+								<img className="rounded-t-lg clippy" src={sampleImage2} alt="" />
+							</a>
+							<div className="p-1">
+								<h5 className="text-green-700 text-2xl font-semibold ">
+									Global Factor
+								</h5>
+								<p className="text-green-700 font-semibold mb-4 text-2xl">
+									Investing Study
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="carousel-item relative float-left w-full">
+					<div className="flex justify-center flex-col m-6 boxshadow">
+						<div className="mb-5">
+							<h2 className="text-white text-2xl">ACME Insights</h2>
+							<p className="text-white">
+								How are factors being used around the world?
+							</p>
+						</div>
+						<div className="shadow-lg bg-white  p-5">
+							<a href="#!">
+								<img className="rounded-t-lg clippy" src={sampleImage2} alt="" />
+							</a>
+							<div className="p-1">
+								<h5 className="text-green-700 text-2xl font-semibold">
+									Global Factor
+								</h5>
+								<p className="text-green-700 text-2xl mb-4 font-semibold">
+									Investing Study
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<button
+				className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+				type="button"
+				data-bs-target="#carouselExampleCaptions"
+				data-bs-slide="prev"
+			>
+				<span
+					className="carousel-control-prev-icon inline-block bg-no-repeat"
+					aria-hidden="true"
+				></span>
+				<span className="visually-hidden">Previous</span>
+			</button>
+			<button
+				className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+				type="button"
+				data-bs-target="#carouselExampleCaptions"
+				data-bs-slide="next"
+			>
+				<span
+					className="carousel-control-next-icon inline-block bg-no-repeat"
+					aria-hidden="true"
+				></span>
+				<span className="visually-hidden">Next</span>
+			</button>
+		</div>
+	);
 };
 
 export default HeroCarousel;
--- src/components/SectionTwo/SectionTwo.js
+++ src/components/SectionTwo/SectionTwo.js
@@ -2,50 +2,162 @@ import sampleImage2 from "../../assets/images/building.png";
 
 import "tw-elements";
 const SectionTwo = () => {
-  return (
-    <div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">
-      <div class="carousel-inner relative w-full overflow-hidden">
-        <div class="carousel-item active relative float-left w-full">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-            class="block w-full"
-            alt="Wild Landscape"
-          />
-        </div>
-        <div class="carousel-item relative float-left w-full">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-            class="block w-full"
-            alt="Camera"
-          />
-        </div>
-        <div class="carousel-item relative float-left w-full">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-            class="block w-full"
-            alt="Exotic Fruits"
-          />
-        </div>
-      </div>
-      <button
-        class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-  );
+	return (
+		<div
+			id="carouselExampleIndicators "
+			class="carousel slide relative bg-gray"
+			data-bs-ride="carousel "
+		>
+			<div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center  p-0 h-80-">
+				<button
+					type="button"
+					data-bs-target="#carouselExampleIndicators"
+					data-bs-slide-to="0"
+					class="active"
+					aria-current="true"
+					aria-label="Slide 1"
+				></button>
+				<button
+					type="button"
+					data-bs-target="#carouselExampleIndicators"
+					data-bs-slide-to="1"
+					aria-label="Slide 2"
+				></button>
+				<button
+					type="button"
+					data-bs-target="#carouselExampleIndicators"
+					data-bs-slide-to="2"
+					aria-label="Slide 3"
+				></button>
+			</div>
+			<div class="carousel-inner relative w-full overflow-hidden ">
+				<div className="mx-5 my-6">
+					<h1 className=" text-3xl text-blue-900 ">
+						Upcoming <span className="font-bold">Events</span>
+					</h1>
+					<p className="text-stone-500 font-medium">
+						At ACME, we're dedicated to learn,ing connecting, and exploring
+						opportunities
+					</p>
+				</div>
+				<div class="carousel-item active float-left w-full relative">
+					{/* <img
+						src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+						class="block w-full"
+						alt="Wild Landscape"
+					/> */}
+					<div className="bg-zinc-300  w-20 h-20 absolute rotate-45	z-0  right-72 ml-15 mt-5 -mr-2 clippy3"></div>
+					<div className="bg-white z-20 w-28 h-20 absolute right-52 top-0">
+						<p className="text-md mt-2 text-center text-emerald-600">
+							JAN <br />
+							<span className="font-extrabold text-3xl">28</span>
+						</p>
+					</div>
+					<div class="flex justify-center relative">
+						<div class="block px-5 m-5 shadow-lg bg-blue-900 max-w-sm pt-16 pb-24 clippy2  relative">
+							<h5 class="text-white text-xl leading-tight font-medium mb-2">
+								Insight Exchange Network
+							</h5>
+							<p class="text-white text-base mb-4">
+								Join us for this conference showcasing innovation.
+							</p>
+							<button
+								type="button"
+								class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+							>
+								View Events Detail
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item float-left w-full ">
+					{/* <img
+						src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+						class="block w-full"
+						alt="Camera"
+					/> */}
+					<div className="bg-zinc-300  w-20 h-20 absolute rotate-45	z-0  right-72 ml-15 mt-5 -mr-2 clippy3"></div>
+					<div className="bg-white z-20 w-28 h-20 absolute right-52 ">
+						<p className="text-md mt-2 text-center text-emerald-600">
+							JAN <br />
+							<span className="font-extrabold text-3xl">28</span>
+						</p>
+					</div>
+					<div class="flex justify-center ">
+						<div class="block px-5 m-5 shadow-lg bg-blue-900 max-w-sm pt-16 pb-24 clippy2">
+							<div>
+								<h5 class="text-white text-xl leading-tight font-medium mb-2">
+									Insight Exchange Network
+								</h5>
+								<p class="text-white text-base mb-4">
+									Join us for this conference showcasing innovation.
+								</p>
+							</div>
+							<button
+								type="button"
+								class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+							>
+								View Events Detail
+							</button>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item float-left w-full ">
+					{/* <img
+						src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+						class="block w-full"
+						alt="Exotic Fruits"
+					/> */}
+					<div className="bg-zinc-300  w-20 h-20 absolute rotate-45	 right-72 ml-15 mt-5 -mr-2 clippy3"></div>
+					<div className="bg-white z-20 w-28 h-20 absolute right-52  ">
+						<p className="text-md mt-2 text-center text-emerald-600">
+							JAN <br />
+							<span className="font-extrabold text-3xl">28</span>
+						</p>
+					</div>
+					<div class="flex justify-center ">
+						<div class="block px-5 m-5 shadow-lg bg-blue-900 max-w-sm pt-16 pb-24 clippy2">
+							<h5 class="text-white text-xl leading-tight font-medium mb-2">
+								Insight Exchange Network
+							</h5>
+							<p class="text-white text-base mb-4">
+								Join us for this conference showcasing innovation.
+							</p>
+							<button
+								type="button"
+								class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+							>
+								View Events Detail
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<button
+				class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0 mt-28 "
+				type="button"
+				data-bs-target="#carouselExampleIndicators"
+				data-bs-slide="prev"
+			>
+				<span
+					class="carousel-control-prev-icon inline-block bg-no-repeat"
+					aria-hidden="true"
+				></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button
+				class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0 mt-28"
+				type="button"
+				data-bs-target="#carouselExampleIndicators"
+				data-bs-slide="next"
+			>
+				<span
+					class="carousel-control-next-icon inline-block bg-no-repeat bg"
+					aria-hidden="true"
+				></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	);
 };
 export default SectionTwo;
